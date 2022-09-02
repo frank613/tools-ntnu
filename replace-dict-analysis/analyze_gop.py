d781b6fdd034f01dbaa191cc2a06a5771715fa54
frank <frankcao613@gmail.com>
2022-09-02 23:00:49 +0200

    replace-dict

---
+++ replace-dict-analysis/analyze_gop.py
@@ -0,0 +1,88 @@
+import matplotlib
+#matplotlib.use('TkAgg')
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import mpld3
+import sys
+import re
+import pandas as pd
+
+re_phone = re.compile(r'([A-Z]+)[0-9]*(_\w)*')
+
+def labelError(GOP_file, cano_GOP, from_phone):
+    gop_df = readGOPToDF(GOP_file)
+    cano_df = readGOPToDF(cano_GOP)
+    df = pd.DataFrame(columns=('phonemes','scores','labels', 'uttid'))
+    if len(gop_df.index) != len(cano_df.index):
+        sys.exit("number of the uttids of the two GOPs is not matching")
+    
+    for index, row in cano_df.iterrows():
+        if row['uttid'] != gop_df.loc[index, "uttid"]:
+            sys.exit("uttid not matching, ignored the utterance")
+        if len(row["seq-score"]) != len(gop_df.loc[index, "seq-score"]):
+            print("length of phonemes not matching for uttid: {}, ignored the utterence".format(row['uttid']))
+            continue
+        labels = [1 if pair[0] == from_phone else 0 for pair in row["seq-score"]]
+        extended = [ pair + (labels[idx], row['uttid']) for idx, pair in enumerate(gop_df.loc[index, 'seq-score'])]
+        df = df.append(pd.DataFrame(extended, columns=['phonemes','scores','labels', 'uttid']))
+    return df
+
+def readGOPToDF(ark_file):
+    in_file = open(ark_file, 'r')
+    df = pd.DataFrame(columns=('uttid', 'seq-score'))
+    isNewUtt = True
+    seq_score = []
+    for line in in_file:
+        line = line.strip()
+        fields = line.split(' ')
+        if isNewUtt:
+            if len(fields) != 1:
+                sys.exit("uttid must be the first line of each utterance")
+            uttid=fields[0]
+            isNewUtt = False
+            continue
+        if line == '':
+            df.loc[len(df.index)] = [uttid, seq_score]
+            isNewUtt = True
+            seq_score = []
+            continue
+        if len(fields) != 3:
+            continue
+        cur_match = re_phone.match(fields[1])
+        if (cur_match):
+            cur_phoneme = cur_match.group(1)
+        else:
+            sys.exit("non legal phoneme found in the gop file")
+        cur_score = float(fields[2]) 
+        seq_score.append((cur_phoneme, cur_score)) 
+    return df
+
+plt.rcParams["figure.autolayout"] = True
+def plot(df_labels, from_phoneme, phoneme):
+    real = df_labels.loc[(df_labels["phonemes"] == phoneme) & (df_labels["labels"] == 0 ), "scores"].to_numpy()
+    substituted = df_labels.loc[(df_labels["phonemes"] == phoneme ) & (df_labels["labels"] == 1),"scores"].to_numpy()
+    fig, ax = plt.subplots(1, 1)
+    if len(real) != 0:
+        ax.hist(real, density=True, range=[-100, 10], bins=100, histtype='stepfilled', alpha=0.5, label='real')
+        ax.hist(real.mean(), density=True, range=[-100, 10], bins=100, histtype='stepfilled', alpha=0.5, label='original-mean')
+        #ax.axvline(real.mean(), 0, 1, color='k', linestyle='dashed', linewidth=2, label='original-mean')
+    if len(substituted) != 0:
+        ax.hist(substituted, density=True, range=[-100, 10], bins=100, histtype='stepfilled', alpha=0.8, label='substituted from {0}'.format(from_phoneme))
+        ax.hist(substituted.mean(), density=True, range=[-100, 10], bins=100, histtype='stepfilled', alpha=0.8, label='wrong-mean')
+        #ax.axvline(substituted.mean(), 0, 1, color='k', linestyle='dashed', linewidth=1, label='error-mean')  
+    ax.legend(loc ="upper left")
+    ax.set_title('phoneme {0}, substitue from {1}'.format(phoneme, from_phoneme))
+
+    html_str = mpld3.fig_to_html(fig)
+    Html_file= open("./output/{0}/{1}.html".format(phoneme, from_phoneme),"w")
+    Html_file.write(html_str)
+    Html_file.close()     
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit("this script takes 4 arguments <GOP file> <cano GOP file> <phoneme-orig> <phoneme-to> . \
+        It labels the substituted phonemes and plot the points with the labels")
+
+    df_labels = labelError(sys.argv[1], sys.argv[2], sys.argv[3])
+    plot(df_labels, sys.argv[3], sys.argv[4])
+    #showGOP(df_gop)
---
+++ replace-dict-analysis/get_phone_from_gop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#convert gop to canonical phone sequence 
+
+gop_cano=./gop.score.all.symbol.orig
+gop_subdir=/localhome/stipendiater/xinweic/kaldi/egs/librispeech/s5/exp/gop_replace_all
+
+dirlist=$(find $gop_subdir -mindepth 1 -maxdepth 1 -type d)
+
+for dir in ${dirlist[*]};do
+	fname=$(basename $dir)
+	echo "prcessing ${fname}"
+	fro_to=(${fname//_/ })
+	if [ ! 2 -eq ${#fro_to[@]} ];then
+		echo "bad-dir-name"
+		continue
+	fi
+	if [ ! -d output/${fro_to[1]} ];then
+		#continue
+		#rm -rf output/${fro_to[1]}
+		mkdir -p output/${fro_to[1]}
+	fi
+	python analyze_gop.py $dir/gop.score.all.symbol  $gop_cano ${fro_to[0]} ${fro_to[1]}
+done
+
+
